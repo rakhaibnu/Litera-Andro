da28a2814795777bff15321df1131179a7c00e84
rakhaibnu <mrakha.gunawan@gmail.com>
2025-05-08 00:06:10 +0700

    Review and NoReview is Done

--- src/pages/NoReview.jsx
+++ src/pages/NoReview.jsx
@@ -3,12 +3,12 @@ import { Link } from "react-router-dom";
 
 export default function NoReview() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 font-merriweather gap-2">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 font-merriweather gap-4">
         <h1 className="text-5xl ">There are no reviews here yet</h1>
         <p className="font-lato">Find a book you love and share your thoughts</p>
         <img src={ImageEmpty} alt="No Reviews" className="w-2xs pt-4" />
         
-        <Link to="/review" className="mt-6 bg-warm-sand-5 hover:bg-warm-sand-9 text-white text-lg px-4 py-2 rounded-full transition duration-300">
+        <Link to="/review" className="mt-6 bg-warm-sand-5 hover:bg-dark text-white text-lg px-4 py-2 rounded-full transition duration-300">
             Go review a book
         </Link>
         </div>
