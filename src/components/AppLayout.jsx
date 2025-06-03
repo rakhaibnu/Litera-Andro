4f6c2dcbf77cee0395ce71d32010e6d181661d64
rakhaibnu <mrakha.gunawan@gmail.com>
2025-06-03 23:13:23 +0700

    layout update

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "./Navbar";
-import Footer from "./Footer";
+import Navbar from "./navbar";
+import Footer from "./footer";
 
 export default function AppLayout() {
     return (
